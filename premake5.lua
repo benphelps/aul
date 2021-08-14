6f3b6842f5ecb61d2b376a998e6b12a13f06ba08
Ben Phelps <ben@phelps.io>
2021-08-14 20:21:57 +0300

    simplify premake steps

--- premake5.lua
+++ premake5.lua
@@ -1,51 +1,56 @@
 local detected_arch = io.popen('uname -m','r'):read('*l')
 
 workspace "Build"
+    language "C++"
+    cppdialect "C++2a"
     configurations { "Debug", "Release" }
     platforms { "intel", "arm" }
     defaultplatform(detected_arch == "x86_64" and "intel" or "arm")
-    filter { "platforms:intel" }
+    filter "platforms:intel"
         architecture "x86_64"
         buildoptions { "-mmacosx-version-min=10.13", "-target x86_64-apple-macos10.13" }
         linkoptions "-target x86_64-apple-macos10.13"
-    filter { "platforms:arm" }
+    filter "platforms:arm"
         architecture "ARM"
         buildoptions { "-mmacosx-version-min=11", "-target arm64-apple-macos11" }
         linkoptions "-target arm64-apple-macos11"
     filter {}
-    objdir "build/obj"
-
-project "Interpreter"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++2a"
-    targetdir "build/%{cfg.architecture}/%{cfg.buildcfg}"
-    includedirs {
-        "include/"
-    }
-    files {
-        "src/main.cpp",
-        "src/**.cpp"
-    }
     filter "configurations:Debug"
         optimize "Off"
     filter "configurations:Release"
         postbuildcommands {
-            "strip -x %{cfg.linktarget.relpath}",
-            "codesign -fs - %{cfg.linktarget.relpath}"
+            "strip -x %{cfg.linktarget.relpath}"
         }
         optimize "Speed"
-        visibility "Hidden"
     filter {}
-
-project "Tests"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++2a"
-    targetdir "build/%{cfg.architecture}/%{cfg.buildcfg}"
     includedirs {
         "include/"
     }
+    objdir "build/obj"
+    targetdir "build/%{cfg.architecture}/%{cfg.buildcfg}"
+
+project "Interpreter"
+    kind "ConsoleApp"
+    files {
+        "src/**.cpp",
+        "src/main.cpp",
+    }
+    excludes {
+        "src/repl.cpp",
+    }
+
+project "Repl"
+    kind "ConsoleApp"
+    files {
+        "src/**.cpp",
+        "src/repl.cpp",
+    }
+    excludes {
+        "src/main.cpp",
+    }
+
+project "Tests"
+    kind "ConsoleApp"
     files {
         "include/catch2/catch_amalgamated.cpp",
         "src/**.cpp",
@@ -53,14 +58,5 @@ project "Tests"
     }
     excludes {
         "src/main.cpp",
+        "src/repl.cpp"
     }
-    filter "configurations:Debug"
-        optimize "Off"
-    filter "configurations:Release"
-        postbuildcommands {
-            "strip -x %{cfg.linktarget.relpath}",
-            "codesign -fs - %{cfg.linktarget.relpath}"
-        }
-        optimize "Speed"
-        visibility "Hidden"
-    filter {}
