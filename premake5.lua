29d86cbf1b0a6d5ea7bbf259a7efea8c11bad6db
Ben Phelps <ben@phelps.io>
2021-08-15 14:25:05 +0300

    simplify build script even further
    remove platform specific details

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,17 @@
-local detected_arch = io.popen('uname -m','r'):read('*l')
-
-workspace "Build"
+workspace "Aul"
     language "C++"
     cppdialect "C++2a"
     configurations { "Debug", "Release" }
-    platforms { "intel", "arm" }
-    defaultplatform(detected_arch == "x86_64" and "intel" or "arm")
-    filter "platforms:intel"
-        architecture "x86_64"
-        buildoptions { "-mmacosx-version-min=10.13", "-target x86_64-apple-macos10.13" }
-        linkoptions "-target x86_64-apple-macos10.13"
-    filter "platforms:arm"
-        architecture "ARM"
-        buildoptions { "-mmacosx-version-min=11", "-target arm64-apple-macos11" }
-        linkoptions "-target arm64-apple-macos11"
-    filter {}
-    filter "configurations:Debug"
-        optimize "Off"
-    filter "configurations:Release"
-        postbuildcommands {
-            "strip -x %{cfg.linktarget.relpath}"
-        }
-        optimize "Speed"
-    filter {}
     includedirs {
         "include/",
         "libs/"
     }
-    objdir "build/obj"
-    targetdir "build/%{cfg.architecture}/%{cfg.buildcfg}"
+    targetdir "build/%{cfg.buildcfg}"
+    filter "configurations:Debug"
+        optimize "Off"
+    filter "configurations:Release"
+        optimize "Speed"
+    filter {}
 
 project "Interpreter"
     kind "ConsoleApp"
