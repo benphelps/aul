c17c7b902fc24bbe9ce370bafd8985fac9059dbc
Ben Phelps <ben@phelps.io>
2022-03-12 12:00:23 +0200

    use readline for repl interaction

--- src/repl.cpp
+++ src/repl.cpp
@@ -1,3 +1,5 @@
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "aul.hpp"
 
 using namespace Aul;
@@ -6,28 +8,28 @@ int main(int argc, char const* argv[])
 {
     Signal::Setup();
 
-    while (true) {
-        std::cout << "aul > ";
+    // disable readline <tab> to autocomplete filenames
+    rl_bind_key('\t', rl_insert);
 
-        string input;
-        std::getline(std::cin, input);
+    std::cout << "Welcome to the Aul REPL! You can exit by pressing Ctrl+C at any time..." << std::endl;
 
-        // handle errors and ctrl + d
-        if (!std::cin) {
-            Signal::Action(SIGQUIT);
-            return 0;
+    char* buf;
+    while ((buf = readline(">> ")) != nullptr) {
+        if (strlen(buf) > 0) {
+            add_history(buf);
         }
 
-        Lexer lexer = Lexer(input);
+        Lexer lexer = Lexer(buf);
         Parser parser = Parser(lexer);
         Program program = parser.parseProgram();
 
-        // for (Token token = lexer.nextToken(); token.type != TokenType::END_OF_FILE; token = lexer.nextToken()) {
-        //     std::cout << token << std::endl;
-        // }
+//        for (Token token = lexer.nextToken(); token.type != TokenType::END_OF_FILE; token = lexer.nextToken()) {
+//            std::cout << token.literal << std::endl;
+//        }
 
         std::cout << program.stringLiteral() << std::endl;
+        std::cout << buf << std::endl;
 
-        std::cout << input << std::endl;
+        free(buf);
     }
 }
